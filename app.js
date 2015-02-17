4f244fe95ddaaebddd1e88f0f1b554f048810a03
Valur Valsson <valurv@nmi.is>
2015-02-17 18:48:23 +0000

    Baseline

---
+++ app.js
@@ -0,0 +1,26 @@
+﻿var app = angular.module('app', ['ui.bootstrap', 'ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partial/home.html'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit       
+        });
+        
+});
+
+
+app.controller('AppCtrl', function ($scope){
+	$scope.pageTitle='Valur';
+})
+
